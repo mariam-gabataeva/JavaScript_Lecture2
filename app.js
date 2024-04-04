35bd1e55cd3200b57ff65bb4bae5bd822a197a3f
mariam-gabataeva <148265916+mariam-gabataeva@users.noreply.github.com>
2024-04-04 13:34:38 +0400

    Change

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ let str = "I found and did my homework";
 let str2 = str.toUpperCase();
 console.log(str2);
 
+// 3
+let mess = Math.floor(Math.random() * 100);
+console.log(mess);
+
 // 4
 let nomber = [1, 2, 3, 4, 5];
 let nomber2 = nomber.reverse();
@@ -22,14 +26,9 @@ let nomber6 = nomber4 % nomber5;
 console.log(nomber6);
 
 // 5
-let keys = {
-  id: 1,
-  name: "Hello",
-};
+let keys = { id: 1, name: "Hello" };
+keys.id = "15";
 let keys2 = Object.keys(keys);
 console.log(keys2);
 let keysArray = Object.entries(keys);
 console.log(keysArray);
-
-// 3. შექმენი ცვლადი, რომელშიც გამოიყენებ Math.floor, Math.random და 1 დან 100 მდე რიცხვს დააბრუნებ.--ვეძებე ამაზე ინფორმაცია, ვიპოვე მაგრამ ვერ გავიგე რა უნდა მექნა.
-// 5. შექმენი ობიექტი, რომელსაც გადასცემ key-დ id და name, ხოლო value-დ 1 და "Hello"-ს, მომდევნო ცვლადში შეინახე პირველი ცვლადის მნიშვნელობა და ამოიღე მისი key Object.key-ს მეშვეობით, ხოლო მესამე ხაზზე ჩაამატე ამ ამოღებულ მასივის ბოლოში რიცხვი 5.--გავაკეთე მე5 დავალება თითქოს მაგრამ აი ეს 5 სად უნდა ჩამესვა ან როგორ ვერ მივხვდი
